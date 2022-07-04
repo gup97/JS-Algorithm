63af218fc6aa93e3e99ee385c5f509b9a519b276
gunhyung <80537765+gup97@users.noreply.github.com>
2022-07-04 12:35:34 +0900

    코드백업자동화

--- acmicpc/백준/단순문제/1000번-A+B/sloution.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-// const filePath = process.platform === "linux" ? "/dev/stdin" : `${__dirname}/input.txt`;
-// let input = fs.readFileSync(filePath).toString().split(" ");
-var input = fs.readFileSync(`${__dirname}/input.txt`).toString().split(" ");
-solution(input);
-
-function solution(input) {
-  console.log(parseint(input[0] + input[1]));
-}
