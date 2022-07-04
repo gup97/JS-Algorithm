700ab6432dad9e22ec35665307a664b4233c8bee
gunhyung <80537765+gup97@users.noreply.github.com>
2022-07-04 12:51:22 +0900

    [Silver IV] Title: 제로, Time: 216 ms, Memory: 22720 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const filePath =  "/dev/stdin" ;
+// 제출시      
+const input = fs.readFileSync(filePath).toString().trim().split(/\n/);
+const N = input[0].split(" ").map(Number);
+const arr = input.splice(1).map(Number);
+
+solution(arr);
+function solution(arr = []) {
+  const stack = [0];
+  for (const el of arr) {
+    if (el === 0) stack.pop();
+    else stack.push(el);
+  }
+  console.log(stack.reduce((a, b) => a + b));
+}
