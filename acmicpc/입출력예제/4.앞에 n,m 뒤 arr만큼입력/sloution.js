003000347edf981b79ff3f3d7d102784d5625fc4
gup97 <gup97@naver.com>
2022-07-05 07:13:40 +0900

    기본 입출력 업데이트

--- acmicpc/입출력예제/4.앞에 n,m 뒤 arr만큼입력/sloution.js
+++ acmicpc/입출력예제/4.앞에 n,m 뒤 arr만큼입력/sloution.js
@@ -5,6 +5,10 @@ const input = fs.readFileSync(filePath).toString().trim().split(/\n/);
 const [N, M] = input[0].split(" ").map(Number);
 const arr = input[1].split(" ").map(Number);
 
+// const input = fs.readFileSync(filePath).toString().trim().split(/\n/);
+// const N = input.splice(0, 1);
+// const arr = input.map((el) => el.replace("\r", ""));
+
 solution(N, M, arr);
 function solution(N, M, arr = []) {
   console.log(arr.filter((el) => M > el).join(" "));
