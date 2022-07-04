7f5d9c69d53bf6b67a1e477b9c233fce6285c364
gunhyung <80537765+gup97@users.noreply.github.com>
2022-07-04 12:32:02 +0900

    [Bronze V] Title: A-B, Time: 124 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : `${__dirname}/input.txt`;
+let input = fs.readFileSync(filePath).toString().split(" ").map(Number);
+
+solution(input);
+
+function solution(input) {
+  console.log(input[0] - input[1]);
+}
