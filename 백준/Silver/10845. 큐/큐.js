28d5f7c722a5b28c591ff7c527747e311fbd46b1
gunhyung <80537765+gup97@users.noreply.github.com>
2022-07-05 09:28:39 +0900

    [Silver IV] Title: 큐, Time: 188 ms, Memory: 12604 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.js
+++ 백준/Silver/10845. 큐/큐.js
@@ -1,38 +1,37 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : `${__dirname}/input.txt`;
-const input = fs.readFileSync(filePath).toString().trim().split(/\n/);
-const N = input.splice(0, 1);
-const arr = input.map((el) => el.replace("\r", ""));
-
-const action = arr.map((el) => {
-  return el.split(" ");
-});
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .split(/\n/)
+  .map((el) => el.replace("\r", ""));
+const N = +input.shift();
 const queue = [];
-let result = "";
-action.forEach((act) => {
+let result = [];
+input.forEach((act) => {
   const isEmpty = (arr) => {
     return arr.length === 0 ? true : false;
   };
-  switch (act[0]) {
-    case "push":
-      queue.push(act[1]);
-      break;
+  switch (act) {
     case "pop":
-      result += `${isEmpty(queue) ? -1 : queue.shift()}\n`;
+      result.push(isEmpty(queue) ? -1 : queue.shift());
       break;
     case "size":
-      result += `${queue.length}\n`;
+      result.push(queue.length);
       break;
     case "empty":
-      result += `${isEmpty(queue) ? 1 : 0}\n`;
+      result.push(isEmpty(queue) ? 1 : 0);
       break;
     case "front":
-      result += `${isEmpty(queue) ? -1 : queue[0]}\n`;
+      result.push(isEmpty(queue) ? -1 : queue[0]);
       break;
     case "back":
-      result += `${isEmpty(queue) ? -1 : queue[queue.length - 1]}\n`;
+      result.push(isEmpty(queue) ? -1 : queue[queue.length - 1]);
+      break;
+    default:
+      queue.push(act.split(" ")[1]);
       break;
   }
 });
 
-console.log(result);
+console.log(result.join("\n"));
