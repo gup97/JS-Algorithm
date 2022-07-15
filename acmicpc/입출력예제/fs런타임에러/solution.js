525742fc3c5ff4e4597a6628b637ab1fc91c9655
gup97 <gup97@naver.com>
2022-07-15 18:24:54 +0900

    오타수정

---
+++ acmicpc/입출력예제/fs런타임에러/solution.js
@@ -0,0 +1,16 @@
+//실행법 이 디렉토리에서 node solution
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+rl.on("line", function (line) {
+  input.push(parseInt(line));
+}).on("close", function () {
+  const x = input[0];
+  const y = input[1];
+  console.log(x > 0 ? (y > 0 ? "1" : "4") : y > 0 ? "2" : "3");
+  process.exit();
+});
--- acmicpc/입출력예제/playground/sloution.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : `${__dirname}/input.txt`;
-
-const input = fs.readFileSync(filePath).toString().trim().split(/\n/);
-const N = input.splice(0, 1);
-const arr = input.map((el) => el.replace("\r", ""));
-
-console.log(arr);
