1f8a09a5e6a410c3f6fd7543e6aff292e74925f5
gunhyung <80537765+gup97@users.noreply.github.com>
2022-07-16 22:28:22 +0900

    [Bronze V] Title: A+B - 8, Time: 124 ms, Memory: 9404 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : `${__dirname}/input.txt`;
+const [T, ...arr] = fs.readFileSync(filePath).toString().trim().split("\n");
+let result = "";
+arr.forEach((el, idx) => {
+  const [A, B] = el.split(" ").map(Number);
+  result += `Case #${idx + 1}: ${A} + ${B} = ${A + B}\n`;
+});
+console.log(result);
