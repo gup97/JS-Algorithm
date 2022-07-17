d0754db3fb86cf3a3de6db81398ac8242a5c66cc
gunhyung <80537765+gup97@users.noreply.github.com>
2022-07-17 22:35:55 +0900

    [Bronze II] Title: 상수, Time: 116 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : `${__dirname}/input.txt`;
+const [A, B] = fs.readFileSync(filePath).toString().trim().split(" ").map(Number);
+
+console.log(Math.max(reverseNum(A), reverseNum(B)));
+
+function reverseNum(params) {
+  return +params.toString().split("").reverse().join("");
+}
