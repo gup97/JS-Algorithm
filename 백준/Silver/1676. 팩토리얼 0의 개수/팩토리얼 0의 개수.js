ac7bd219e810236979af6760165928172c485219
gunhyung <80537765+gup97@users.noreply.github.com>
2022-08-30 21:54:25 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 132 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : `${__dirname}/input.txt`;
+const input = +fs.readFileSync(filePath).toString().trim().split("\n");
+
+function Factorial(input) {
+    let answer = 0;
+    if (input === 0) answer;
+    for (let i = 1; i <= input; i++) {
+        if (i % 5 === 0) answer++;
+        if (i % 25 === 0) answer++;
+        if (i % 125 === 0) answer++;
+    }
+    return answer;
+}
+
+console.log(Factorial(input));
